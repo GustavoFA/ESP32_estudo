1ebf9e343aaa6910097f8f9f4a370f223f777a11
Gustavo Freitas Alves <58485863+GustavoFA@users.noreply.github.com>
2021-10-28 17:45:28 -0300

    Update adc.ino

--- adc.ino
+++ adc.ino
@@ -11,7 +11,7 @@ void setup() {
    * width_bit = ADC_WIDTH_X, resolução do ADC em que x = BIT_y (-> y = 9, 10, 11 e 12), MAX
    */
   adc1_config_width(ADC_WIDTH_BIT_12);
-
+  // Para o ADC2 não utiliza essa função, pois, ela é configurada a cada vez que você ler o valor da GPIO (logo, ela é configurada em adc2_get_raw)
   // esp_err_t adc1_config_channel_atten(adc1_channel_t channel, adc_atten_t atten)
   // channel = ADC1_CHANNEL_z, z = número de GPIO que está ligada ao canal 1
   // atten = ADC_ATTEN_DB_w, w = 0, 2_5, 6, 11
